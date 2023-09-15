67de4c2d5c12232ebc3d12c5f7b72f21e6797713
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-09-15 11:56:06 -0500

    Boring dropdown

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('T')
+function toggleDropdown(button) {
+    const menu = button.nextElementSibling; // Get the next element (dropdown menu)
+  
+    // Toggle the 'show' class to display/hide the menu
+    if (menu.style.display === 'block') {
+      menu.style.display = 'none';
+    } else {
+      menu.style.display = 'block';
+    }
+  }
+  
+  // Add event listeners to all dropdown toggles with the class 'dropdown-toggle'
+  const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+  
+  dropdownToggles.forEach(function (toggle) {
+    toggle.addEventListener('mouseenter', function () {
+      toggleDropdown(this); // Pass the clicked button as an argument
+    });
+  
+    // Optionally, you can add hover behavior by changing the event listener to 'mouseenter'
+    // toggle.addEventListener('mouseenter', function () {
+    //   toggleDropdown(this);
+    // });
+  });
